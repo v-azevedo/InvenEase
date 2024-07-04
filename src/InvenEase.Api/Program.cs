4d8535468ef245187abb2d05f1867dd7525ff66b
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-04 17:27:27 -0300

    feat: add application layer responsible dependency injection

--- src/InvenEase.Api/Program.cs
+++ src/InvenEase.Api/Program.cs
@@ -1,9 +1,9 @@
-using InvenEase.Application.Services.Authentication;
+using InvenEase.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddApplication();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/InvenEase.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using InvenEase.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvenEase.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+}
