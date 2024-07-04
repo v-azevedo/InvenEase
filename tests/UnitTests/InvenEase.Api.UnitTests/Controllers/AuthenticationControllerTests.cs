cfbd8f840255f3ab19c0c8d12370297ea77cc3ff
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-04 17:08:24 -0300

    test: add test for register and login response with correct values

--- tests/UnitTests/InvenEase.Api.UnitTests/Controllers/AuthenticationControllerTests.cs
+++ tests/UnitTests/InvenEase.Api.UnitTests/Controllers/AuthenticationControllerTests.cs
@@ -1,5 +1,7 @@
 using FakeItEasy;
+using FluentAssertions;
 using InvenEase.Api.Controllers;
+using InvenEase.Application.Services.Authentication;
 using InvenEase.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,11 +9,18 @@ namespace InvenEase.Api.UnitTests.Controllers;
 
 public class AuthenticationControllerTests
 {
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationControllerTests()
+    {
+        _authenticationService = A.Fake<IAuthenticationService>();
+    }
+
     [Fact]
     public void AuthenticationController_Register_ReturnAuthenticationResult()
     {
         // Arrange
-        var controller = new AuthenticationController();
+        var controller = new AuthenticationController(_authenticationService);
         var request = A.Fake<RegisterRequest>();
 
         // Act
@@ -22,10 +31,46 @@ public class AuthenticationControllerTests
     }
 
     [Fact]
+    public void AuthenticationController_Register_ReturnAuthenticationResultWithCorrectValues()
+    {
+        // Arrange
+        var controller = new AuthenticationController(_authenticationService);
+        var request = new RegisterRequest(
+            "firstName",
+            "lastName",
+            "email",
+            "password"
+        );
+        var authResult = new AuthenticationResult(
+            Guid.NewGuid(),
+            "firstName",
+            "lastName",
+            "email",
+            "requester",
+            "token"
+        );
+
+        A.CallTo(() => _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password
+        )).Returns(authResult);
+
+        // Act
+        var result = controller.Register(request) as OkObjectResult;
+        var response = result!.Value as AuthenticationResponse;
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Should().BeEquivalentTo(authResult, options => options.Excluding(o => o.Id));
+    }
+
+    [Fact]
     public void AuthenticationController_Login_ReturnAuthenticationResult()
     {
         // Arrange
-        var controller = new AuthenticationController();
+        var controller = new AuthenticationController(_authenticationService);
         var request = A.Fake<LoginRequest>();
 
         // Act
@@ -33,5 +78,37 @@ public class AuthenticationControllerTests
 
         // Assert
         Assert.IsType<OkObjectResult>(result);
+    }
+
+    [Fact]
+    public void AuthenticationController_Login_ReturnAuthenticationResultWithCorrectValues()
+    {
+        // Arrange
+        var controller = new AuthenticationController(_authenticationService);
+        var request = new LoginRequest(
+            "email",
+            "password"
+        );
+        var authResult = new AuthenticationResult(
+            Guid.NewGuid(),
+            "firstName",
+            "lastName",
+            "email",
+            "requester",
+            "token"
+        );
+
+        A.CallTo(() => _authenticationService.Login(
+            request.Email,
+            request.Password
+        )).Returns(authResult);
+
+        // Act
+        var result = controller.Login(request) as OkObjectResult;
+        var response = result!.Value as AuthenticationResponse;
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Should().BeEquivalentTo(authResult, options => options.Excluding(o => o.Id));
     }
 }
