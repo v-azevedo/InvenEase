218fce66e59ae07142a2af2a6bc3dc0a795a5fa6
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-04 17:32:44 -0300

    feat: add layer based dependency injection
    In this commit will add for the application and the infrastructure layer

--- src/InvenEase.Api/Program.cs
+++ src/InvenEase.Api/Program.cs
@@ -1,6 +1,12 @@
+using InvenEase.Application;
+using InvenEase.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- src/InvenEase.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace InvenEase.Infrastructure;
-
-public class Class1
-{
-
-}
