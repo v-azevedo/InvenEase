1c37dfd482e54b7d2ae1a5c78085dfd604baad0b
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-05 15:18:32 -0300

    feat: implement necessary services for the jwt token generator

--- src/InvenEase.Infrastructure/DependencyInjection.cs
+++ src/InvenEase.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,20 @@
+using InvenEase.Application.Common.Interfaces.Authentication;
+using InvenEase.Application.Common.Interfaces.Services;
+using InvenEase.Infrastructure.Authentication;
+using InvenEase.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InvenEase.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
         return services;
     }
 }
