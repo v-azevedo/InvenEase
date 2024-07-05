79001b73b8433080f25ccc9009fdc8c96e697020
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-05 15:17:48 -0300

    test: update test to use role enum

--- tests/UnitTests/InvenEase.Application.UnitTests/Services/Authentication/AuthenticationServiceTests.cs
+++ tests/UnitTests/InvenEase.Application.UnitTests/Services/Authentication/AuthenticationServiceTests.cs
@@ -1,15 +1,25 @@
+using FakeItEasy;
 using FluentAssertions;
+using InvenEase.Application.Common.Interfaces.Authentication;
 using InvenEase.Application.Services.Authentication;
+using InvenEase.Domain.Common;
 
 namespace InvenEase.Application.UnitTests.Services.Authentication;
 
 public class AuthenticationServiceTests
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationServiceTests()
+    {
+        _jwtTokenGenerator = A.Fake<IJwtTokenGenerator>();
+    }
+
     [Fact]
     public void AuthenticationService_Login_ReturnAuthenticationResult()
     {
         // Arrange
-        var service = new AuthenticationService();
+        var service = new AuthenticationService(_jwtTokenGenerator);
 
         // Act
         var result = service.Login("test@email.com", "password");
@@ -23,23 +33,27 @@ public class AuthenticationServiceTests
     public void AuthenticationService_Register_ReturnAuthenticationResult()
     {
         // Arrange
-        var service = new AuthenticationService();
+        var service = new AuthenticationService(_jwtTokenGenerator);
+        var userId = Guid.NewGuid();
+        var password = "password";
         var expected = new AuthenticationResult(
-            Guid.NewGuid(),
+            userId,
             "john",
             "doe",
             "john.doe@email.com",
-            "requester",
+            Role.Requester,
             "token");
 
         // Act
-        var result = service.Register("john", "doe", "john.doe@email.com", "password");
+        var result = service.Register(expected.FirstName, expected.LastName, expected.Email, password);
 
         // Assert
-        result.Should().NotBeNull();
         result.Should().BeOfType<AuthenticationResult>();
         result.Should().BeEquivalentTo(expected, options =>
-            options.Excluding(o => o.Id)
+            options
+                .Excluding(o => o.Id)
+                .Excluding(o => o.Token)
         );
+        result.Id.Should().NotBeEmpty();
     }
 }
