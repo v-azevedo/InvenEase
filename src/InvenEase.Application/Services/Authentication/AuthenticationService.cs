d765f72e42423ce3ebdc05c5600c47aa349cc766
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-05 15:26:55 -0300

    feat: add jwt token generator for the register method

--- src/InvenEase.Application/Services/Authentication/AuthenticationService.cs
+++ src/InvenEase.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,17 @@
+using InvenEase.Application.Common.Interfaces.Authentication;
+using InvenEase.Domain.Common;
+
 namespace InvenEase.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(
@@ -9,18 +19,22 @@ public class AuthenticationService : IAuthenticationService
             "firstName",
             "lastName",
             email,
-            "requester",
+            Role.Requester,
             "token");
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+
+        var userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName, Role.Requester);
+
         return new AuthenticationResult(
-            Guid.NewGuid(),
+            userId,
             firstName,
             lastName,
             email,
-            "requester",
-            "token");
+            Role.Requester,
+            token);
     }
 }
