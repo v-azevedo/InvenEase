52d21a1308c51c6c6e4a666f18827d9fe71bee6c
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-16 13:46:21 -0300

    refactor: update mapping to use Mapster with mapper

--- src/InvenEase.Api/Controllers/AuthenticationController.cs
+++ src/InvenEase.Api/Controllers/AuthenticationController.cs
@@ -3,29 +3,27 @@ using InvenEase.Application.Authentication.Commands.Register;
 using InvenEase.Application.Authentication.Common;
 using InvenEase.Application.Authentication.Queries.Login;
 using InvenEase.Contracts.Authentication;
+using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InvenEase.Api.Controllers;
 
 [Route("auth")]
-public class AuthenticationController(ISender mediator) : ApiController
+public class AuthenticationController(ISender mediator, IMapper mapper) : ApiController
 {
     private readonly ISender _mediator = mediator;
+    private readonly IMapper _mapper = mapper;
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterRequest request)
     {
-        var command = new RegisterCommand(
-            request.FirstName,
-            request.LastName,
-            request.Email,
-            request.Password);
+        var command = _mapper.Map<RegisterCommand>(request);
 
         ErrorOr<AuthenticationResult> authResult = await _mediator.Send(command);
 
         return authResult.Match(
-            authResult => Ok(MapAuthenticationResult(authResult)),
+            authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
             errors => Problem(errors)
         );
     }
@@ -33,26 +31,13 @@ public class AuthenticationController(ISender mediator) : ApiController
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest request)
     {
-        var query = new LoginQuery(
-            request.Email,
-            request.Password);
+        var query = _mapper.Map<LoginQuery>(request);
 
         ErrorOr<AuthenticationResult> authResult = await _mediator.Send(query);
 
         return authResult.Match(
-             authResult => Ok(MapAuthenticationResult(authResult)),
+             authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
              errors => Problem(errors)
          );
-    }
-
-    private static AuthenticationResponse MapAuthenticationResult(AuthenticationResult authResult)
-    {
-        return new AuthenticationResponse(
-            authResult.User.Id,
-            authResult.User.FirstName,
-            authResult.User.LastName,
-            authResult.User.Email,
-            authResult.User.Role.ToString(),
-            authResult.Token);
     }
 }
