7cb3940879239891764b66bde32f9f6d73768ce5
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-17 14:09:05 -0300

    feature: implements validation behavior with FluentValidations

--- src/InvenEase.Application/DependencyInjection.cs
+++ src/InvenEase.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using FluentValidation;
+using InvenEase.Application.Common.Behaviors;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InvenEase.Application;
@@ -8,6 +11,9 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
