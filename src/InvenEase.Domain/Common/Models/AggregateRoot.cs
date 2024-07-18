eb5fb059438ac884488fdff75f2f08a8c443f628
Victor Azevedo <132946058+v-azevedo@users.noreply.github.com>
2024-07-18 15:45:42 -0300

    Feature/AggregateRoot-Entity-ValueObject (#6)
    * feature: implements aggregate root class
    
    * feature: implements entity class
    
    * feature: implements value object class

---
+++ src/InvenEase.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,9 @@
+namespace InvenEase.Domain.Common.Models;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id)
+    {
+    }
+}
