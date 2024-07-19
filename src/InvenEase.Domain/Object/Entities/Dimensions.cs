51c7f4f17866e399eeac43c18ec8f167bc08cfe8
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-19 00:38:03 -0300

    feat: implements object domain model

---
+++ src/InvenEase.Domain/Object/Entities/Dimensions.cs
@@ -0,0 +1,34 @@
+
+using InvenEase.Domain.Common.Models;
+
+namespace InvenEase.Domain.Object.Entities;
+
+public sealed class Dimensions : ValueObject
+{
+    public double Length { get; private set; }
+    public double Width { get; private set; }
+    public double Height { get; private set; }
+    public double Weight { get; private set; }
+
+    private Dimensions(double length, double width, double height, double weight)
+    {
+        Length = length;
+        Width = width;
+        Height = height;
+        Weight = weight;
+    }
+
+    public static Dimensions CreateNew(double length, double width, double height, double weight)
+    {
+        return new Dimensions(length, width, height, weight);
+    }
+
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Length;
+        yield return Width;
+        yield return Height;
+        yield return Weight;
+    }
+}
---
+++ src/InvenEase.Domain/Object/Object.cs
@@ -0,0 +1,65 @@
+using InvenEase.Domain.Common.Models;
+using InvenEase.Domain.Object.Entities;
+using InvenEase.Domain.Request.ValueObjects;
+
+namespace InvenEase.Domain.Object;
+
+public sealed class Object : AggregateRoot<ObjectId>
+{
+    private readonly List<RequestId> _requestsList = [];
+    private readonly List<OrderId> _ordersList = [];
+    public string Name { get; }
+    public string Description { get; }
+    public string Code { get; }
+    public string Image { get; }
+    public Dimensions Dimensions { get; }
+    public int Quantity { get; }
+    public int MinimumQuantity { get; }
+    public DateTime CreatedDateTime { get; }
+    public DateTime UpdatedDateTime { get; }
+
+    private Object(
+        ObjectId id,
+        string name,
+        string description,
+        string code,
+        string image,
+        Dimensions dimensions,
+        int quantity,
+        int minimumQuantity,
+        DateTime createdDateTime,
+        DateTime updatedDateTime) : base(id)
+    {
+        Name = name;
+        Description = description;
+        Code = code;
+        Image = image;
+        Dimensions = dimensions;
+        Quantity = quantity;
+        MinimumQuantity = minimumQuantity;
+        CreatedDateTime = createdDateTime;
+        UpdatedDateTime = updatedDateTime;
+    }
+
+    public static Object Create(
+        string name,
+        string description,
+        string code,
+        string image,
+        Dimensions dimensions,
+        int quantity,
+        int minimumQuantity)
+    {
+        return new Object(
+            ObjectId.CreateUnique(),
+            name,
+            description,
+            code,
+            image,
+            dimensions,
+            quantity,
+            minimumQuantity,
+            DateTime.UtcNow,
+            DateTime.UtcNow);
+    }
+}
