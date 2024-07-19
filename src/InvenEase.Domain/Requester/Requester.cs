9cb244e16cbae2022b15d0efe025d559525581af
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-19 00:36:56 -0300

    feat: implements requester domain model

---
+++ src/InvenEase.Domain/Requester/Requester.cs
@@ -0,0 +1,52 @@
+using InvenEase.Domain.Common.Enums;
+using InvenEase.Domain.Common.Models;
+using InvenEase.Domain.Request.ValueObjects;
+
+namespace InvenEase.Domain.Requester;
+
+public sealed class Requester : AggregateRoot<RequesterId>
+{
+    private readonly List<RequestId> _requestsList = [];
+
+    public string FirstName { get; }
+    public string LastName { get; }
+    public Role Role { get; }
+    public string ProfileImage { get; }
+    public UserId UserId { get; }
+    public IReadOnlyList<RequestId> RequestsList =>
+        _requestsList.AsReadOnly();
+    public DateTime CreatedDateTime { get; }
+    public DateTime UpdatedDateTime { get; }
+
+    private Requester(
+        RequesterId id,
+        string firstName,
+        string lastName,
+        Role role,
+        string profileImage,
+        UserId userId,
+        DateTime createdDateTime,
+        DateTime updatedDateTime) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Role = role;
+        ProfileImage = profileImage;
+        UserId = userId;
+        CreatedDateTime = createdDateTime;
+        UpdatedDateTime = updatedDateTime;
+    }
+
+    public static Requester Create(string firstName, string lastName, string profileImage, UserId userId)
+    {
+        return new Requester(
+            RequesterId.CreateUnique(),
+            firstName,
+            lastName,
+            Role.Requester,
+            profileImage,
+            userId,
+            DateTime.UtcNow,
+            DateTime.UtcNow);
+    }
+}
---
+++ src/InvenEase.Domain/Requester/ValueObjects/RequesterId.cs
@@ -0,0 +1,23 @@
+using InvenEase.Domain.Common.Models;
+
+namespace InvenEase.Domain.Request.ValueObjects;
+
+public sealed class RequesterId : ValueObject
+{
+    public Guid Value { get; }
+
+    private RequesterId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static RequesterId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
