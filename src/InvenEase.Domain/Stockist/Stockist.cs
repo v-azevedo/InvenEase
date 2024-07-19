d25e492c23c18e40455577d01360db489674cedf
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-19 00:36:43 -0300

    feat: implements stockist domain model

---
+++ src/InvenEase.Domain/Stockist/Stockist.cs
@@ -0,0 +1,52 @@
+using InvenEase.Domain.Common.Enums;
+using InvenEase.Domain.Common.Models;
+using InvenEase.Domain.Request.ValueObjects;
+
+namespace InvenEase.Domain.Stockist;
+
+public sealed class Stockist : AggregateRoot<StockistId>
+{
+    private readonly List<RequestId> _requestsList = [];
+
+    public string FirstName { get; }
+    public string LastName { get; }
+    public Role Role { get; }
+    public string ProfileImage { get; }
+    public UserId UserId { get; }
+    public IReadOnlyList<RequestId> RequestsList =>
+        _requestsList.AsReadOnly();
+    public DateTime CreatedDateTime { get; }
+    public DateTime UpdatedDateTime { get; }
+
+    private Stockist(
+        StockistId id,
+        string firstName,
+        string lastName,
+        Role role,
+        string profileImage,
+        UserId userId,
+        DateTime createdDateTime,
+        DateTime updatedDateTime) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Role = role;
+        ProfileImage = profileImage;
+        UserId = userId;
+        CreatedDateTime = createdDateTime;
+        UpdatedDateTime = updatedDateTime;
+    }
+
+    public static Stockist Create(string firstName, string lastName, string profileImage, UserId userId)
+    {
+        return new Stockist(
+            StockistId.CreateUnique(),
+            firstName,
+            lastName,
+            Role.Stockist,
+            profileImage,
+            userId,
+            DateTime.UtcNow,
+            DateTime.UtcNow);
+    }
+}
