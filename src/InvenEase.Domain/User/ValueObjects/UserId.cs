2afe5975426615195b9ab036bae7026414af6a01
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-19 00:36:33 -0300

    feat: implements user domain model

---
+++ src/InvenEase.Domain/User/ValueObjects/UserId.cs
@@ -0,0 +1,23 @@
+using InvenEase.Domain.Common.Models;
+
+namespace InvenEase.Domain.Request.ValueObjects;
+
+public sealed class UserId : ValueObject
+{
+    public Guid Value { get; }
+
+    private UserId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static UserId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
