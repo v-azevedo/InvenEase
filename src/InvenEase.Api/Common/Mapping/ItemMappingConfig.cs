94772912ecd83c86982fc56588b695710a479e54
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-25 19:25:03 -0300

    feat: add mapping configuration for the update item request to the command

--- src/InvenEase.Api/Common/Mapping/ItemMappingConfig.cs
+++ src/InvenEase.Api/Common/Mapping/ItemMappingConfig.cs
@@ -1,4 +1,5 @@
 using InvenEase.Application.Items.Commands.CreateItem;
+using InvenEase.Application.Items.Commands.UpdateItem;
 using InvenEase.Contracts.Items;
 using InvenEase.Domain.ItemAggregate;
 
@@ -10,11 +11,16 @@ public class ItemMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<CreateItemRequest, CreateItemCommand>();
+        config.NewConfig<CreateItemRequest, CreateItemCommand>()
+            .Map(dest => dest, src => src);
 
         config.NewConfig<Item, ItemResponse>()
             .Map(dest => dest.Id, src => src.Id.Value)
             .Map(dest => dest.RequestIds, src => src.RequestIds.Select(requestId => requestId.Value))
             .Map(dest => dest.OrderIds, src => src.OrderIds.Select(orderId => orderId.Value));
+
+        config.NewConfig<(UpdateItemRequest request, Guid Id), UpdateItemCommand>()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.Item, src => src.request);
     }
 }
