be6824eebc9992b1e70ef1df07d1377b92ac566e
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-29 21:21:17 -0300

    feat: implements user repository with db context

---
+++ src/InvenEase.Application/Common/Interfaces/Persistence/IUsersRepository.cs
@@ -0,0 +1,12 @@
+using InvenEase.Domain.UserAggregate;
+
+namespace InvenEase.Application.Common.Interfaces.Persistence;
+
+public interface IUsersRepository
+{
+    Task AddAsync(User user, CancellationToken cancellationToken);
+    Task<User?> GetByIdAsync(Guid userId, CancellationToken cancellationToken);
+    Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken);
+    Task RemoveAsync(User user, CancellationToken cancellationToken);
+    Task UpdateAsync(User user, CancellationToken cancellationToken);
+}
