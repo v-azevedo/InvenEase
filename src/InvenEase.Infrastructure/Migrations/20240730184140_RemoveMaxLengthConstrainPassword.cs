72121fb82c7abf78f0e3ffb630e9140847313af1
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-30 15:48:02 -0300

    chore: add migration to remove length constraint from password field

---
+++ src/InvenEase.Infrastructure/Migrations/20240730184140_RemoveMaxLengthConstrainPassword.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvenEase.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMaxLengthConstrainPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
