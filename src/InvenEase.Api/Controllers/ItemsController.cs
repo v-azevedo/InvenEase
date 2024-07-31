3e77ebbcf205f9e8d3fed11c919e379b6d90d489
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-31 15:12:24 -0300

    refactor: remove readonly fields

--- src/InvenEase.Api/Controllers/ItemsController.cs
+++ src/InvenEase.Api/Controllers/ItemsController.cs
@@ -13,45 +13,42 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InvenEase.Api.Controllers;
 
-[Route("/items")]
+[Route("[controller]")]
 public class ItemsController(IMapper mapper, ISender mediator) : ApiController
 {
-    private readonly IMapper _mapper = mapper;
-    private readonly ISender _mediator = mediator;
-
     [HttpPost]
     public async Task<IActionResult> CreateItem(CreateItemRequest request)
     {
-        var command = _mapper.Map<CreateItemCommand>(request);
+        var command = mapper.Map<CreateItemCommand>(request);
 
-        var createItemResult = await _mediator.Send(command);
+        var createItemResult = await mediator.Send(command);
 
         return createItemResult.Match(
-            item => Ok(_mapper.Map<ItemResponse>(item)),
+            item => Ok(mapper.Map<ItemResponse>(item)),
             errors => Problem(errors));
     }
 
     [HttpPut("{id:Guid}")]
     public async Task<IActionResult> UpdateItem(Guid id, UpdateItemRequest request)
     {
-        var command = _mapper.Map<UpdateItemCommand>((request, id));
+        var command = mapper.Map<UpdateItemCommand>((request, id));
 
-        var updateItemResult = await _mediator.Send(command);
+        var updateItemResult = await mediator.Send(command);
 
         return updateItemResult.Match(
-            item => Ok(_mapper.Map<ItemResponse>(item)),
+            item => Ok(mapper.Map<ItemResponse>(item)),
             errors => Problem(errors));
     }
 
     [HttpGet("{id:Guid}")]
     public async Task<IActionResult> GetItem(Guid id)
     {
-        var query = _mapper.Map<GetItemQuery>(id);
+        var query = mapper.Map<GetItemQuery>(id);
 
-        var getItemResult = await _mediator.Send(query);
+        var getItemResult = await mediator.Send(query);
 
         return getItemResult.Match(
-            item => Ok(_mapper.Map<ItemResponse>(item)),
+            item => Ok(mapper.Map<ItemResponse>(item)),
             errors => Problem(errors));
     }
 
@@ -60,19 +57,19 @@ public class ItemsController(IMapper mapper, ISender mediator) : ApiController
     {
         var query = new GetItemsQuery();
 
-        var getItemsResult = await _mediator.Send(query);
+        var getItemsResult = await mediator.Send(query);
 
         return getItemsResult.Match(
-            items => Ok(_mapper.Map<IEnumerable<ItemResponse>>(items)),
+            items => Ok(mapper.Map<IEnumerable<ItemResponse>>(items)),
             Problem);
     }
 
     [HttpDelete("{id:Guid}")]
     public async Task<IActionResult> DeleteItem(Guid id)
     {
-        var command = _mapper.Map<DeleteItemCommand>(id);
+        var command = mapper.Map<DeleteItemCommand>(id);
 
-        var deleteItemResult = await _mediator.Send(command);
+        var deleteItemResult = await mediator.Send(command);
 
         return deleteItemResult.Match(
             _ => NoContent(),
