ee8c93badcd2c48102aaa06bf18fc174e21fc858
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-07-31 01:09:02 -0300

    feat: add property to hold the item id

--- src/InvenEase.Domain/RequestAggregate/Entities/RequestItem.cs
+++ src/InvenEase.Domain/RequestAggregate/Entities/RequestItem.cs
@@ -1,23 +1,28 @@
 using InvenEase.Domain.Common.Models;
+using InvenEase.Domain.ItemAggregate.ValueObjects;
 using InvenEase.Domain.RequestAggregate.ValueObjects;
 
 namespace InvenEase.Domain.RequestAggregate.Entities;
 
 public sealed class RequestItem : Entity<RequestItemId>
 {
+    public ItemId ItemId { get; private set; }
     public int Quantity { get; private set; }
 
-    public RequestItem(
-        RequestItemId id,
-        int quantity) : base(id)
+    private RequestItem(
+        RequestItemId requestItemId,
+        ItemId itemId,
+        int quantity) : base(requestItemId)
     {
         Quantity = quantity;
+        ItemId = itemId;
     }
 
-    public static RequestItem Create(int quantity)
+    public static RequestItem Create(int quantity, ItemId itemId)
     {
         return new RequestItem(
             RequestItemId.CreateUnique(),
+            itemId,
             quantity);
     }
 
