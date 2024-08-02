4ebd33a5ff7d5ba2f8c1efa4b271ea93c4b452e6
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-08-02 15:17:05 -0300

    feat: implements requisition repo with db context

--- src/InvenEase.Application/Common/Interfaces/Persistence/IRequisitionRepository.cs
+++ src/InvenEase.Application/Common/Interfaces/Persistence/IRequisitionRepository.cs
@@ -5,4 +5,5 @@ namespace InvenEase.Application.Common.Interfaces.Persistence;
 public interface IRequisitionRepository
 {
     Task CreateAsync(Requisition requisition, CancellationToken cancellationToken);
+    Task<List<Requisition>> GetAllAsync(CancellationToken cancellationToken);
 }
--- src/InvenEase.Infrastructure/Requisitions/Repository/RequisitionsRepository.cs
+++ src/InvenEase.Infrastructure/Requisitions/Repository/RequisitionsRepository.cs
@@ -2,6 +2,8 @@ using InvenEase.Application.Common.Interfaces.Persistence;
 using InvenEase.Domain.RequisitionAggregate;
 using InvenEase.Infrastructure.Persistence;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace InvenEase.Infrastructure.Requisitions.Repository;
 
 public class RequisitionsRepository(InvenEaseDbContext dbContext) : IRequisitionRepository
@@ -10,5 +12,10 @@ public class RequisitionsRepository(InvenEaseDbContext dbContext) : IRequisition
     {
         await dbContext.AddAsync(requisition, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<List<Requisition>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await dbContext.Requisitions.ToListAsync(cancellationToken);
     }
 }
