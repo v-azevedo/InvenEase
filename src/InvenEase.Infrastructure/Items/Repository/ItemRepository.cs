128728e5f14a5cd95934ee6e38af440c163a3f1b
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-08-02 15:20:33 -0300

    refactor: update to use only the primary constructor

--- src/InvenEase.Infrastructure/Items/Repository/ItemRepository.cs
+++ src/InvenEase.Infrastructure/Items/Repository/ItemRepository.cs
@@ -9,33 +9,31 @@ namespace InvenEase.Infrastructure.Items.Repository;
 
 public class ItemRepository(InvenEaseDbContext dbContext) : IItemRepository
 {
-    private readonly InvenEaseDbContext _dbContext = dbContext;
-
     public async Task CreateAsync(Item item, CancellationToken cancellationToken)
     {
-        await _dbContext.AddAsync(item, cancellationToken);
-        await _dbContext.SaveChangesAsync(cancellationToken);
+        await dbContext.AddAsync(item, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
     }
 
     public async Task UpdateAsync(Item item, CancellationToken cancellationToken)
     {
-        _dbContext.Update(item);
-        await _dbContext.SaveChangesAsync(cancellationToken);
+        dbContext.Update(item);
+        await dbContext.SaveChangesAsync(cancellationToken);
     }
 
     public async Task<Item?> GetByIdAsync(ItemId id, CancellationToken cancellationToken)
     {
-        return await _dbContext.Items.FirstOrDefaultAsync(item => item.Id == id, cancellationToken);
+        return await dbContext.Items.FirstOrDefaultAsync(item => item.Id == id, cancellationToken);
     }
 
     public async Task<List<Item>> GetAllAsync(CancellationToken cancellationToken)
     {
-        return await _dbContext.Items.ToListAsync(cancellationToken);
+        return await dbContext.Items.ToListAsync(cancellationToken);
     }
 
     public async Task DeleteAsync(Item item, CancellationToken cancellationToken)
     {
-        _dbContext.Remove(item);
-        await _dbContext.SaveChangesAsync(cancellationToken);
+        dbContext.Remove(item);
+        await dbContext.SaveChangesAsync(cancellationToken);
     }
 }
