509d4d651e9da50fc3057d183c015d6b20866bde
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-08-02 15:19:22 -0300

    chore: add latest migrations

--- src/InvenEase.Infrastructure/Migrations/20240731185437_UpdateRequestsNames.Designer.cs
+++ src/InvenEase.Infrastructure/Migrations/20240731185437_UpdateRequestsNames.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InvenEase.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace InvenEase.Infrastructure.Migrations
 {
     [DbContext(typeof(InvenEaseDbContext))]
-    partial class InvenEaseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240731185437_UpdateRequestsNames")]
+    partial class UpdateRequestsNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -84,7 +87,7 @@ namespace InvenEase.Infrastructure.Migrations
                     b.Property<int>("Status")
                         .HasColumnType("integer");
 
-                    b.Property<Guid?>("StockistId")
+                    b.Property<Guid>("StockistId")
                         .HasColumnType("uuid");
 
                     b.Property<DateTime>("UpdatedDateTime")
